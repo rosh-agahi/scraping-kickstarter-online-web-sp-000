ee6a73ad64b7a2fe90c336157ffdce3959baf235
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-29 22:15:49 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-require_relative '../kickstarter_scraper.rb'
+require_relative '../kickstarter_scraper'
 
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
