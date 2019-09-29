e290906043cdfaae0214548a60625d3a7ad84093
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-29 22:12:34 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,10 @@
 # require libraries/modules here
 
+require Nokigiri
+
+
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+ 
+  kickstarter = Nokogiri::HTML(html)
 end
